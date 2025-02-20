92ed2a345f9f53820bb551cc9ac6565ca5087710
Andreas Madsack <andreas@madflex.de>
2025-02-20 21:05:11 +0100

    bump to version 0.5

--- src/datasette_webhook_write/__init__.py
+++ src/datasette_webhook_write/__init__.py
@@ -8,7 +8,7 @@ from datasette import hookimpl
 from datasette.utils import sqlite3
 from datasette.utils.asgi import Response
 
-__version__ = "0.4"
+__version__ = "0.5"
 
 
 def check_signature(sig, data, secret, digestmod):
